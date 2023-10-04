a81d4b96f954e8295ba8c9b9734c8dddb131d82e
Emmanuel Londono <123216952+emmanuel-londono@users.noreply.github.com>
2023-10-04 16:00:26 -0600

    Add .env webdriver location

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from dotenv import load_dotenv
+
+load_dotenv()
+WEBDRIVER_FILEPATH = os.getenv('WEBDRIVER_FILEPATH')
 website = "http://localhost:5555/"
-driver_path = 'C:/Users/emman/chrome-win64'
-driver = webdriver.Chrome(path)
+
+driver = webdriver.Chrome(WEBDRIVER_FILEPATH)
 driver.get(website)
 result = requests.get(website)
 
