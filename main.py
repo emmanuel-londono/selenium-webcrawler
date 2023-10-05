b8147601a21b9f490ca80fa7cd0c93b458e67f21
Emmanuel Londono <emmanuel.londono@us.af.mil>
2023-10-05 11:48:56 -0600

    add exported scrapped-table-names to folder, add .ts file inside folder

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-websites = ["http://localhost:5555/","https://expressjs.com/en/5x/api.html#req"]
-browser = webdriver.Chrome(options=chrome_options)
-browser.get(websites[0])
+import pandas as pd
+import os
+
+websites = ["http://localhost:5555/","https://expressjs.com/en/5x/api.html#req", "https://nvd.nist.gov/"]
+driver = webdriver.Chrome()
+driver.get(websites[0])
+driver.implicitly_wait(0.5)
+# "//input[@value='f']"
+cveList = driver.find_elements(By.XPATH, "//ul[@id='latestVulns']")
+tables = driver.find_elements(By.XPATH,"//li[@class='_model_1xigg_60']")
+titles = []
+
+for element in tables:
+    titles.append(element.find_element(By.XPATH, "./div[1]").text)
+driver.quit()
+#   context.append(element.find_element(By.XPATH, "/#text")).text
+#   published.append(element.find_element(By.XPATH, "/strong[2]")).text
+df = pd.DataFrame({'title':titles})
+df.to_csv('')
+print(df)
+
+base_directory = ""
+
+for folder_name in titles:
+    #Create new folder name
+    folder_path = os.path.join(base_directory, folder_name)
+    os.makedirs(folder_path)
+    with open(os.path.join(folder_path,f"{folder_name}.router.ts"),'w'):
+        pass
