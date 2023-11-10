36d58f372ab2749efc965f48f1e442eb1ac2a526
Emmanuel Londono <emmanuel.londono@us.af.mil>
2023-11-10 11:36:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,31 @@ tables = driver.find_elements(By.XPATH,"//li[@class='_model_1xigg_60']")
 titles = []
 
 for element in tables:
+    if(element.find_element(By.XPATH, "./div[1]").text == "aor" or "site"):
+        pass
     titles.append(element.find_element(By.XPATH, "./div[1]").text)
 driver.quit()
 #   context.append(element.find_element(By.XPATH, "/#text")).text
 #   published.append(element.find_element(By.XPATH, "/strong[2]")).text
-df = pd.DataFrame({'title':titles})
-df.to_csv('')
-print(df)
 
-base_directory = ""
+standardEnpoints = []
+path = 'path'
+router = 'router'
+standardRouter = 'standardRouter'
 
-for folder_name in titles:
-    #Create new folder name
-    folder_path = os.path.join(base_directory, folder_name)
-    os.makedirs(folder_path)
-    with open(os.path.join(folder_path,f"{folder_name}.router.ts"),'w'):
-        pass
+for title in titles :
+    standardEnpoints.append( { path: f"/{title}", router: standardRouter })
+print(standardEnpoints)
+
+# df = pd.DataFrame({'title':titles})
+# df.to_csv('')
+# print(df)
+
+# base_directory = ""
+
+# for folder_name in titles:
+#     #Create new folder name
+#     folder_path = os.path.join(base_directory, folder_name)
+#     os.makedirs(folder_path)
+#     with open(os.path.join(folder_path,f"{folder_name}.router.ts"),'w'):
+#         pass
